96d6cebcd55b3a4c25b4a22e7bc9c1733d8ef3cf
Fyodor Soikin <name.fa@gmail.com>
2018-05-27 23:51:12 -0400

    Remove config file, move to Argu.

--- server/src/Program.fs
+++ server/src/Program.fs
@@ -3,7 +3,7 @@
 open Suave
 open Suave.Filters
 open Suave.Operators
-open Microsoft.Extensions.Configuration
+open Argu
 
 [<AutoOpen>]
 module Json =
@@ -14,22 +14,9 @@ module FS =
     let home = System.AppContext.BaseDirectory
     let resolve segments = System.IO.Path.Combine( Array.ofList <| home::segments ) |> System.IO.Path.GetFullPath
 
-module Config =
-    type Config = { Root: string; Debug: bool }
-    let config =
-        let root = ConfigurationBuilder().SetBasePath(FS.home).AddJsonFile("appsettings.json").Build()
-        { Root = root.["Root"]; Debug = root.["Debug"] = "true" }
-
 let homeDir = FS.resolve ["Content"]
-let root = FS.resolve [Config.config.Root]
 
-let files =
-    if Config.config.Debug then
-        Writers.setHeader "Cache-Control" "max-age=1" >=> context (fun c -> Files.browseHome)
-    else
-        Files.browseHome
-
-let getLogs (procs:string, ticks) =
+let getLogs root (procs:string, ticks) =
     let logs =
         [for proc in procs.Split ',' do
             let log = Process.Ops.log root proc (System.DateTime ticks)
@@ -37,11 +24,17 @@ let getLogs (procs:string, ticks) =
         ]
     Successful.OK << toJson <| logs
 
-let app =
+let app root debug =
+    let files =
+        if debug then
+            Writers.setHeader "Cache-Control" "max-age=1" >=> context (fun _ -> Files.browseHome)
+        else
+            Files.browseHome
+
     choose [
         GET  >=> path "/api/list"          >=> request (fun _ -> Process.Stats.list root |> toJson |> Successful.OK)
         POST >=> path "/api/gc"            >=> request (fun _ -> Process.Ops.collectGarbage root |> toJson |> Successful.OK)
-        GET  >=> pathScan "/api/log/%s/%d" getLogs
+        GET  >=> pathScan "/api/log/%s/%d" (getLogs root)
         POST >=> pathScan "/api/start/%s"  (Process.Ops.start root >> toJson >> Successful.OK)
         POST >=> pathScan "/api/stop/%s"   (Process.Ops.stop root >> toJson >> Successful.OK)
 
@@ -52,18 +45,48 @@ let app =
         RequestErrors.NOT_FOUND "Not Found"
     ]
 
-let bindingsFromPort p =
-    match Option.map Sockets.Port.TryParse p with
-    | Some (true, p) -> [ HttpBinding.create HTTP System.Net.IPAddress.Loopback p ]
-    | _ -> defaultConfig.bindings
+let run root port debug =
+    printfn "HomeDir = %s" homeDir
+    printfn "Root = %s" root
+
+    if not (System.IO.Directory.Exists root) then
+        printfn "Directory '%s' doesn't exist" root
+        1
+    else
+        app root debug
+        |> startWebServer
+            { defaultConfig with
+                homeFolder = Some homeDir
+                bindings =
+                    match port with
+                    | Some p -> [ HttpBinding.create HTTP System.Net.IPAddress.Loopback p ]
+                    | _ -> defaultConfig.bindings
+            }
+        0
+
+
+[<AutoOpen>]
+module Options =
+    open Argu
+
+    type Options =
+        | [<ExactlyOnce>] Root of path:string
+        | [<AltCommandLine "-p"; Unique>] Port of port:System.UInt16
+        | [<AltCommandLine "-d"; Unique>] Debug
+    with interface IArgParserTemplate with
+            member s.Usage =
+                match s with
+                | Root _ -> "Root directory, where the `procs` file is located"
+                | Port _ -> "Port on which to listen (if not specified will be chosen automatically)"
+                | Debug -> "Debug mode"
+
+    let parser = ArgumentParser.Create<Options>(programName = "vapor")
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "HomeDir = %s" homeDir
-    app
-    |> startWebServer
-        { defaultConfig with
-            homeFolder = Some homeDir
-            bindings = bindingsFromPort (Seq.tryHead argv)
-        }
-    0
+    try
+        let opts = Options.parser.Parse argv
+        run (opts.GetResult <@Root@>) (opts.TryGetResult <@Port@>) (opts.Contains <@Debug@>)
+    with
+        | :? ArguParseException as e -> printfn "%s" e.Message; 1
